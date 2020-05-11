a20e3ffd33ca11b267f48608b7b601df9415981d
Wojciech Matusiak <wm382710@students.mimuw.edu.pl>
2020-05-11 13:25:12 +0200

    Something concerning functions calls

---
+++ Intermediate.hs
@@ -0,0 +1,129 @@
+module Intermediate where
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Data.Maybe (isJust, fromJust, isNothing)
+import qualified Control.Monad.State.Strict as MS
+import Control.DeepSeq
+
+type InterpIO = MS.StateT ProgramState IO
+data ProgramState = PS {
+  stateMemory :: Memory,
+  stateGlobalFrame :: Frame
+}
+
+------------------------------------ MEMORY ------------------------------------
+data VType = IntegerType | BoolType | StringType
+data VData = VInt Int | VBool Bool | VStr String
+instance NFData VData where
+  rnf (VInt v) = v `seq` ()
+  rnf (VBool v) = v `seq` ()
+  rnf (VStr v) = v `seq` ()
+data Variable = Var Int VData
+type Memory = Map.Map Integer Variable
+type Frame = Map.Map String Integer
+
+find :: Memory -> Integer -> Variable
+find memory id =
+  let store = Map.lookup id memory in 
+  if isJust store then 
+    fromJust store
+  else 
+    error "Dereference deleted variable"
+
+updateMemory :: (Memory -> Memory) -> InterpIO ()
+updateMemory modify = do
+  state <- MS.get
+  MS.put (state {stateMemory = modify (stateMemory state)})
+  return ()
+
+putRef :: VData -> InterpIO Integer
+putRef = undefined
+
+incRef :: Integer -> InterpIO ()
+incRef id = do
+    state <- MS.get
+    let (Var refc val) = find (stateMemory state) id
+    updateMemory (\m -> Map.insert id (Var (refc + 1) val) m)
+    return ()
+  
+decRef :: Integer -> InterpIO ()
+decRef id = do
+  state <- MS.get
+  let (Var refc val) = find (stateMemory state) id
+  if refc > 1 then do
+    updateMemory (\m -> Map.insert id (Var (refc - 1) val) m)
+    return ()
+  else do
+    updateMemory (\m -> Map.delete id m)
+    return ()
+  
+updateRef :: Integer -> (VData -> VData) -> InterpIO ()
+updateRef id operation = do
+  state <- MS.get
+  let (Var refc val) = find (stateMemory state) id
+  updateMemory (\m -> Map.insert id (Var (refc + 1) (operation val)) m)
+  return ()
+
+---------------------------------- FUNCTIONS -----------------------------------
+data ArgPassType = PassRef | PassVal
+data Arg = Value VData | Reference Integer
+type RFIArg = (String, ArgPassType)
+type FunctionBody = Frame -> ByteCode
+data RuntimeFunctionInfo = RFI {
+  fName :: String,
+  fBoundVariables :: [String],
+  fFreeVariables :: [String],
+  fLocalFunctions :: Map.Map String RuntimeFunctionInfo,
+  fArgsTypes :: [RFIArg],
+  fReturnType :: VType,
+  fBody :: FunctionBody
+}
+
+type ByteCode = InterpIO VData
+
+updateFrame :: Frame -> [RFIArg] -> [Arg] -> InterpIO Frame
+updateFrame f [] [] = do return f
+updateFrame f ((name, PassRef):ais) ((Reference id):as) = do
+  incRef id
+  let f' = Map.insert name id f
+  f'' <- updateFrame f' ais as
+  return f''
+updateFrame f ((name, PassVal):ais) ((Value val):as) = do
+  id <- putRef val
+  f' <- updateFrame f ((name, PassRef):ais) ((Reference id):as)
+  return f'
+
+shutdownFrame :: Frame -> [RFIArg] -> InterpIO ()
+shutdownFrame f [] = do return ()
+shutdownFrame f ((name, _):ais) = do
+  let id = fromJust (Map.lookup name f)
+  decRef id
+  shutdownFrame f ais
+  return ()
+
+applyArgs :: FunctionBody -> Frame -> [RFIArg] -> InterpIO ([Arg] -> ByteCode)
+applyArgs body closure argInfo = 
+  return (\args -> do
+    frame <- updateFrame closure argInfo args
+    retval <- frame `deepseq` (body frame)
+    retval `deepseq` (shutdownFrame frame argInfo)
+    return retval)
+
+instantiateFunction :: RuntimeFunctionInfo -> Frame -> InterpIO ([Arg] -> ByteCode)
+instantiateFunction rfi parentFrame =
+  let
+    referVars initFrame (v:vs) = do
+      let refMaybe = Map.lookup v parentFrame
+      if isJust refMaybe then do
+        let ref = fromJust refMaybe
+        incRef ref
+        frame <- (referVars (Map.insert v ref initFrame) vs)
+        return frame
+      else do
+        error "[BUG] instantiateFunction: free variable dereference failed"
+    referVars initFrame [] = do return initFrame
+  in do
+  state <- MS.get
+  newFrame <- referVars Map.empty (fFreeVariables rfi)
+  res <- applyArgs (fBody rfi) newFrame (fArgsTypes rfi)
+  return res
--- Main.hs
+++ Main.hs
@@ -1,12 +1,13 @@
 import qualified Tokenizer
 import qualified System.IO as IO
-import Tokenizer as Tkz
-import Parser
 import qualified Data.Map as Map
 import Control.Monad.State.Strict as State
 import Control.DeepSeq
 import System.Environment (getArgs)
 import Data.List (intercalate)
+import Tokenizer as Tkz
+import Parser
+import qualified Intermediate
 
 main = do 
   args <- getArgs
